4e8eca4f353baf6a44626ee86dde90b0888d070e
Ngozi Stephen <ngozi.stephen99@gmail.com>
2022-06-07 02:48:45 +0100

    worked on edit and preview

---
+++ resources/views/admin/contacts/edit.blade.php
@@ -0,0 +1,76 @@
+<!-- Modal For Edit contact-->
+<div class="modal fade" id="exampleModalEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+                <form method="post" id="editContactForm" action=" ">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Edit Admin Details</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <input  name="address" id="address" class="form-control" style="margin-bottom: 30px" placeholder="Address">
+
+                        <div class="form-group">
+                            <div class="input-group" >
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                                </div>
+                                <input type="text" id="phone_numberEdit" name="phone_number" class="form-control"data-inputmask="'mask': ['999-999-9999 [x99999]', '+99 99 99 9999[9]-9999']" data-mask="" placeholder="Phone Number">
+                            </div>
+                            <!-- /.input group -->
+                        </div>
+                    
+                        <div class="form-group">
+                            <textarea  id="compose-textareaEdit" class="form-control" style="height: 100px">
+                            </textarea>
+                        </div>
+
+
+                        <div class="form-group" id="social_medialinks">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    Social Media Company:
+                                </div>
+                                <div class="col-md-6">
+                                    Link:
+                                </div>
+                            </div>
+                            @for ($i=0; $i <= 3; $i++)
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="text" name="social_medialinks[{{ $i }}][key]" class="form-control" value="{{ old('social_medialinks['.$i.'][key]') }}">
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" name="social_medialinks[{{ $i }}][value]" class="form-control" value="{{ old('social_medialinks['.$i.'][value]') }}">
+                                </div>
+                            </div>
+                            @endfor
+                        </div>
+
+                        <div class="form-group">
+                            <div class="btn btn-default btn-file">
+                                <i class="fas fa-paperclip"></i> Profile Image
+                                <input id="featuredImgInput" accept="image/*" type="file" name="profile_img">
+                            </div>
+
+                            <label class="d-block" for="featuredImgInput"></label>
+                            <p class="help-block">Max. 32MB</p>
+                        </div>
+
+                    
+                    </div>
+
+                    
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" form="editContactForm" class="btn btn-primary">Save</button>
+                    </div>
+
+                    @csrf 
+                </form>
+        </div>
+    </div>
+</div> 
---
+++ resources/views/admin/contacts/preview.blade.php
@@ -0,0 +1,71 @@
+<style>
+    .flex-container{
+          display: flex;
+        background-color:#bd5d38;
+          
+
+        }
+</style>
+
+
+<div class="modal fade" id="exampleModalPreview" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+                <form action="#" method="post" id="createPreviewContactForm">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">{{Auth::user()->firstname }}  {{Auth::user()->lastname }}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        
+                        <div class="flex-container">
+
+                                <div class="card-header" >
+                                    <img class="user-info" id="featuredImgInputpreview" style="border-radius: 50%"></img>
+                                </div>
+                                <div class="card-header" style="align-items: center">
+                                    <h3 class="user-info" id="">{{Auth::user()->firstname }}  {{Auth::user()->lastname }}</h3>
+                            
+                                    <h3 class="user-info" id="emailpreview"></h3>
+                                </div>
+                        </div>
+
+
+                        <div class="card-header">
+                            <div>Phone</div>
+                            <h3 class="user-info" id="phone_numberpreview"></h3>
+
+                            <div>Address</div>
+
+                            <h3 class="user-info" id="addresspreview"></h3>
+                        </div>
+                        <!-- /.card-header --> 
+                        <div class="card-body">
+                            <div>Tell Us About yourself?</div>
+                            <div class="mt-5" id="compose-textareapreview">
+                               
+                            </div>
+                            
+                        </div> 
+                        <div class="card-header">
+                            <div>Social Media</div>
+                            <h3 class="user-info" id="social_medialinkspreview"></h3>
+
+                        </div>
+                                          
+                    </div>
+
+                    
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" form="createPreviewContactForm" class="btn btn-primary">Save</button>
+                    </div>
+
+                    @csrf 
+                </form>
+        </div>
+    </div>
+</div> 
