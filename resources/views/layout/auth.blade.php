4869bd7069c4aad7de25a18a5526429525eef369
Ngozi Stephen <ngozi.stephen99@gmail.com>
2022-10-17 10:31:01 +0100

    remove adminlte

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -13,13 +13,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/adminLTE/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="/adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/adminLTE/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
@@ -43,11 +43,11 @@
    @yield('content')
 
     <!-- jQuery -->
-    <script src="/adminLTE/plugins/jquery/jquery.min.js"></script>
+    <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="/adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="/adminLTE/dist/js/adminlte.min.js"></script>
+    <script src="/adminlte/dist/js/adminlte.min.js"></script>
 
 
 {{-- from laravel ui
